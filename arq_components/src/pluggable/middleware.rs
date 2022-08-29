f3a64ddf702cf418a39b5a87af56600412dc511a
MagTheDev <jtulek@at-rt.com>
2022-08-29 18:34:06 +0200

    Add Fairing wrapper, add some docs

--- arq_components/src/pluggable/middleware.rs
+++ arq_components/src/pluggable/middleware.rs
@@ -1,12 +1,11 @@
 
-use std::{any::Any, mem};
-use rocket::fairing::{Fairing, self};
+use std::{mem, any::Any};
+use rocket::{fairing::Fairing, Request, Data, Response};
 
 
 
 /// This trait is used when one wants to add middleware to the ARQ_CORE.
 /// This acts as a request guard and so should be used with caution, as it could interfiere with the base CORE routes.
-
 pub trait MiddlewareComponent: Any + Send + Sync {
     /// This function should return the name of the middleware.
     fn name(&self) -> &'static str;
@@ -27,16 +26,22 @@ pub struct MiddlewareFactory {
 }
 
 impl MiddlewareFactory {
-
+    /// Construct a new MiddlewareFactory
     pub fn new() -> Self {
         MiddlewareFactory { middlewares: Vec::new() }
     }
-
+    /// Add a single middleware to be exported
     pub fn add_middleware(mut self, middleware: Box<dyn Fairing>) -> Self {
         self.middlewares.push(middleware);
         self
     }
-
+    /// Add multiple middlewares to be exported
+    pub fn add_middlewares(mut self, middlewares: Vec<Box<dyn Fairing>>) -> Self {
+        self.middlewares.extend(middlewares);
+        self
+    }
+    /// Export all the middlewares to be mounted by CORE
+    /// This consumes the factory
     pub fn export(mut self) -> (*mut Box<dyn Fairing>, usize, usize) {
 
 
@@ -54,3 +59,30 @@ impl MiddlewareFactory {
 
 }
 
+/// A struct that will act as a wrapper for `dyn Fairing` since rocket cannot attach `dyn Trait` as a middleware
+/// This should only be used by the ComponentManager, users do not need to worry about this
+pub struct DynFairing {
+    inner: Box<dyn Fairing>
+}
+
+impl DynFairing {
+    /// Constructs a new DynFairing
+    pub fn from(inner: Box<dyn Fairing>) -> Self {
+        Self {
+            inner
+        }
+    }   
+}
+
+#[rocket::async_trait]
+impl Fairing for DynFairing {
+    fn info(&self) -> rocket::fairing::Info {
+        self.inner.info()
+    }
+    async fn on_request(&self, request: &mut Request<'_>, data: &mut Data<'_>) {
+        self.inner.on_request(request, data);
+    }
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        self.inner.on_response(request, response);
+    }
+}
