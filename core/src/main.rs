a37807937bfb93634c07d649689de16cb543d965
MagTheDev <jtulek@at-rt.com>
2022-09-25 16:46:44 +0200

    Change core to use prelude from arq_plugins

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,10 +1,9 @@
-use arq_components::pluggable::manager::ComponentManager;
-
+use arq_plugins::prelude::*;
 
 #[tokio::main]
 async fn main() {
     unsafe {
-        let mut manager = ComponentManager::new();
+        let mut manager = PluginManager::new();
         manager.load_middleware("./target/debug/libsample_middleware.so").unwrap();
 
         let middlewares = manager.get_middlewares();
